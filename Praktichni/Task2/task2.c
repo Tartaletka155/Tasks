6893f07edd2b299882673c1eeec59c12b4f10d4e
Tobiz223 <bogdanmuliarchuk1234@gmail.com>
2025-07-08 18:03:33 +0300

    поправки

---
+++ Praktichni/Task2/task2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+int main() {
+    int n;
+    printf("Введіть кількість чисел: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        printf("Введіть число %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    int result = arr[0];
+    for (int i = 1; i < n; i++) {
+        result = lcm(result, arr[i]);
+    }
+
+    printf("НСК: %d\n", result);
+    return 0;
+}
