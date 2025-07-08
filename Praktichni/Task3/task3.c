19124e66b24e492c32370f2a739ef528e41d992e
Tobiz223 <bogdanmuliarchuk1234@gmail.com>
2025-07-08 18:19:52 +0300

    Змінення

---
+++ Praktichni/Task3/task3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int p;
+    printf("Введіть p: ");
+    scanf("%d", &p);
+
+    long long a[p+1], b[p+1];
+    a[1] = b[1] = 1;
+    a[2] = b[2] = 2;
+
+    for (int i = 3; i <= p; i++) {
+        a[i] = a[i-1] + b[i-1];
+        b[i] = a[i-1] + b[i-1] + a[i-2] + b[i-2];
+    }
+
+    printf("Кількість чисел: %lld\n", a[p] + b[p]);
+    return 0;
+}
